f2e3c11c5416d0070644894c184edb2946c496c5
Hasankhan170 <hasankhanali0316@gmail.com>
2024-11-05 11:48:46 +0500

    some changes in app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Login from './pages/Login.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Register from './pages/Register.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element : <App/>,
+    children : [
+      {
+        path  :'login',
+        element : <Login/>
+      },
+      {
+        path  :'register',
+        element : <Register/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <RouterProvider router={router}>
     <App />
-  </StrictMode>,
+  </RouterProvider>,
 )
